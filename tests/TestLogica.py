21195bdef1fa202d8b0f04543cfa62e0e82de2f5
Juan David Torres <jd.torres2@uniandes.edu.co>
2023-03-09 21:03:52 -0500

    Prueba 4 TERMINADO

--- tests/TestLogica.py
+++ tests/TestLogica.py
@@ -15,5 +15,8 @@ class ExampleTestCase(unittest.TestCase):
     	#Prueba para verificar que el caso funciona. El nombre del método usa el prefijo test_
 	def test_nombre_claves_01(self):
 		self.assertEqual(1, 1)
+    
+    def test_nombre_claves_02(self):
+		self.assertEqual(1, 1)
 		
     
